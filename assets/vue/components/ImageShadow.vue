fa18f9a43649eaaa7433942fdbf898465c986b6a
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-06-03 08:22:17 +0200

    Also added assets
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

---
+++ assets/vue/components/ImageShadow.vue
@@ -0,0 +1,86 @@
+<template>
+    <component :is="tag">
+        <img :src="img" :alt="alt" class="image--shadow-background" :class="classList">
+    </component>
+</template>
+
+<script>
+    export default {
+        props: {
+            link: {
+                type: String,
+            },
+            img: {
+                type: String,
+                required: true
+            },
+            alt: {
+                type: String,
+                required: false,
+                default: 'Wonen op Tetterode sfeerfoto'
+            },
+            background: {
+                type: String,
+                required: false,
+                default: 'primary',
+                validator: (val) => ['primary', 'secondary'].indexOf(val) !== -1
+            },
+            left: {
+                type: Boolean,
+                default: false
+            },
+            classes: {
+                type: Array,
+                default: () => []
+            }
+        },
+        computed: {
+            tag() {
+                return (!this.link) ? 'section' : 'a'
+            },
+            classList() {
+                this.classes.push(`image--shadow-${this.background}`)
+                if (this.left) {
+                    this.classes.push('image--shadow-left')
+                }
+                
+                return this.classes.join(' ')
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    $colors: (
+        'primary': #FF6700,
+        'secondary': #0092A5
+    );
+    
+    $shadowOffset: 25;
+    
+    .image--shadow-background {
+        position: relative;
+        max-width: calc(100% - #{$shadowOffset}px);
+        
+        &:not(.image--shadow-left) {
+            @each $name, $color in $colors {
+                &.image--shadow-#{$name} {
+                    box-shadow: #{$shadowOffset}px #{$shadowOffset}px $color
+                }
+            }
+        }
+        
+        /*For the left shadow*/
+        &.image--shadow-left {
+            margin-left: auto;
+            
+            @each $name, $color in $colors {
+                &.image--shadow-#{$name} {
+                    box-shadow: -#{$shadowOffset}px #{$shadowOffset}px $color
+                }
+            }
+
+        }
+    }
+
+</style>
---
+++ assets/vue/components/TitleStriped.vue
@@ -0,0 +1,76 @@
+<template>
+    <component :is="size" class="heading-striped" :class="classList">
+        <slot></slot>
+    </component>
+</template>
+
+<script>
+    export default {
+        name: "TitleStriped",
+        
+        props: {
+            size: {
+                type: String,
+                default: 'h3',
+                validator: (val) => ['h1', 'h2', 'h3', 'h4'].indexOf(val) !== -1
+            },
+            
+            lineColor: {
+                type: String,
+                default: 'secondary',
+                validator: (val) => ['primary', 'secondary'].indexOf(val) !== -1
+            },
+            
+            textColor: {
+                type: String,
+                default: 'primary',
+                validator: (val) => ['primary', 'secondary'].indexOf(val) !== -1
+            },
+            
+            classes: {
+                type: Array,
+                default: () => []
+            }
+        },
+        
+        computed: {
+            classList() {
+                this.classes.push(`${this.lineColor}--lining`)
+                this.classes.push(`colored--${this.textColor}`)
+                
+                return this.classes.join(' ')
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    $colors: (
+        'primary': #FF6700,
+        'secondary': #0092A5
+    );
+    
+    $fontStart: 3rem;
+    $fontDelta: 0.25rem;
+    
+    @for $i from 1 through 6 {
+        h#{$i} {
+            font-size: $fontStart - $i * $fontDelta;
+        }
+    }
+    
+    .heading-striped {
+        border-bottom-width: 3px;
+        border-style: solid;
+        
+        @each $name, $color in $colors {
+            &.#{$name}--lining {
+                border-color: $color;
+            }
+            
+            &.colored--#{$name} {
+                color: $color;
+            }
+        }
+    }
+</style>
