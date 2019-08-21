321e28b116d365a1076d53bd7dfc6ba0e1c102af
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2019-08-21 20:13:34 +0200

    Fix update URL

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ require_once get_stylesheet_directory() . '/vendor/aristath/kirki/kirki.php';
 require_once get_stylesheet_directory() . '/vendor/autoload.php';
 
 $updates = Puc_v4_Factory::buildUpdateChecker(
-	'https://github.com/DoedeJaarsmaCommunicatie/probable-octo.git',
+	'https://github.com/DoedeJaarsmaCommunicatie/probable-octo',
 	__FILE__,
 	'tetterode'
 );
